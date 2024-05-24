2d6e39ebd8378aeb370cb8ff0c5beec6856b1cb1
Deepthi-Ramesh <deepthimr1411@gmail.com>
2024-05-24 18:06:08 +0530

    vercel json

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 const express =require('express')
 const app=express()
 var cors = require('cors');
-const routes = require('./routes')
-const {hashing} =require('./bcrypt');
-const { mail } = require('./nodemailer');
+const routes = require('./routes');
 app.use(cors());
 app.use(express.json())
 app.use('/', routes);
