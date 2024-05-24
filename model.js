16cdb1330c26a02544b57026eab12f1776a35004
Deepthi-Ramesh <deepthimr1411@gmail.com>
2024-05-24 18:46:16 +0530

    connection updated

--- model.js
+++ model.js
@@ -4,7 +4,8 @@ const connection = mysql.createConnection({
     host: 'sql12.freesqldatabase.com',
     user: 'sql12708706',
     password:'XKRMDIST2B',
-    database:'sql12708706'
+    database:'sql12708706',
+    connectTimeout: 10000
 })
   
 connection.connect((err)=>{
