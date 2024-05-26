3be8bad2b3e157ce0e86834e769603b3e38250c0
Deepthi-Ramesh <deepthimr1411@gmail.com>
2024-05-26 11:44:51 +0530

    port updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const routes = require('./routes');
 app.use(cors());
 app.use(express.json())
 app.use('/', routes);
-
-app.listen(5000,()=>{
+var port = process.env.PORT|| 5000;
+app.listen(port,()=>{
   console.log("Hello server");
 })
