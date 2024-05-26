9c4ecdf4e4a0d8c349a453f7b39251220e061c99
Deepthi-Ramesh <deepthimr1411@gmail.com>
2024-05-26 16:07:19 +0530

    added cors

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express =require('express')
 const app=express()
 var cors = require('cors');
 const routes = require('./routes');
-app.use(cors());
+app.use(cors({
+  origin: 'https://rentify-frontend-coral.vercel.app'
+}));
 app.use(express.json())
 app.use('/', routes);
 app.get('/',(req,res)=>{
